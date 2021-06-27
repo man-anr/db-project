0e4f7e2410fadf8364cccbba5d8003ecb250e45b
man-anr <niafellows@gmail.com>
2021-06-28 01:36:26 +0800

    Updated admin dashboard

--- admin-dashboard/admin.controller.php
+++
@@ -1,9 +0,0 @@
-<?php      
-        include('../database/config.php');
-
-
-         
-
-        echo $_GET['username'];
-        echo "<script>console.log('Username: ".  $_GET['username'] . "');</script>" ;
-?>  
